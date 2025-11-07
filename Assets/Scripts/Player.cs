b08f328447b6338dedcf3ca7fb84cecc5bb2c9d5
Khayle Torres <torreskhayle@gmail.com>
2025-11-07 16:00:03 +0000

    Added simple player controls

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float rotationSpeed = 10f;
+    private void Update()
+    {
+        Vector2 inputVector = new Vector2();
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = 1;
+        }
+
+        inputVector.Normalize();
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+        transform.position += moveDir * Time.deltaTime * moveSpeed;
+
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
+
+        Debug.Log("Input Vector: " + inputVector + " " + transform.forward);
+    }
+}

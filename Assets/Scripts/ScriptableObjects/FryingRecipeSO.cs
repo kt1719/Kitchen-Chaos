a41094030ed6278661481d9b035d9b62ff683cd3
kt1719 <torreskhayle@gmail.com>
2025-12-10 00:10:28 +0000

    Add FryingRecipeSO

---
+++ Assets/Scripts/ScriptableObjects/FryingRecipeSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FryingRecipeSO")]
+public class FryingRecipeSO : ScriptableObject
+{
+    public KitchenObjectSO input;
+    public KitchenObjectSO output;
+    public float fryingTimerMax;
+}

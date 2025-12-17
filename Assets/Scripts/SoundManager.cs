7795fb722d6d1b3e40235a75a09def46119a994c
Khayle Torres <torreskhayle@gmail.com>
2025-12-17 16:20:56 +0000

    Add intial options & msuci + SFX volumes

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,9 +1,21 @@
 using System;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    private const string PLAYER_PREFS_SOUND_EFFECTS_VOLUME = "SFXVolume";
+    public static SoundManager Instance { get; private set; }
     [SerializeField] private AudioClipRefsSO audioClipRefsSO;
+
+    private float volume = 1f;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        volume = PlayerPrefs.GetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME, 1f);
+    }
 
     private void Start()
     {
@@ -56,12 +68,29 @@ public class SoundManager : MonoBehaviour
         PlaySound(audioClipRefsSO.deliverySuccess, DeliveryCounter.Instance.transform.position);
     }
 
-    private void PlaySound(AudioClip[] audioCliparray, Vector3 position, float volume = 1f)
+    private void PlaySound(AudioClip[] audioCliparray, Vector3 position, float volumeMultiplier = 1f)
     {
-        PlaySound(audioCliparray[UnityEngine.Random.Range(0, audioCliparray.Length)], position, volume);
+        PlaySound(audioCliparray[UnityEngine.Random.Range(0, audioCliparray.Length)], position, volumeMultiplier * volume);
     }
     private void PlaySound(AudioClip audioClip, Vector3 position, float volume = 1f)
     {
         AudioSource.PlayClipAtPoint(audioClip, position, volume);
+    }
+
+    public void ChangeVolume()
+    {
+        volume += 0.1f;
+        if (volume > 1f)
+        {
+            volume = 0f;
+        }
+
+        PlayerPrefs.SetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME, volume);
+        PlayerPrefs.Save();
+    }
+
+    public float GetVolume()
+    {
+        return volume;
     }
 }
